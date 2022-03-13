36f4dc3b688668cb78a4d71f12b6e45a3260d405
libredesarrollo <desarrollolibre.net@gmail.com>
2022-03-13 15:43:30 -0400

    proyecto

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from elements.models import Element
+
 # Create your models here.
+class Comment(models.Model):
+    text = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    element = models.ForeignKey(Element, related_name='comments', on_delete=models.CASCADE, null=True)
+ 
+    def __str__(self):
+        return 'Comentario #{}'.format(self.id)
--- elements/models.py
+++ elements/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    url_clean = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class Type(models.Model):
+    title = models.CharField(max_length=255)
+    url_clean = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class Element(models.Model):
+    title = models.CharField(max_length=255)
+    url_clean = models.SlugField(max_length=255,blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10,decimal_places=2, default=6.10) # 12345678.10
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
