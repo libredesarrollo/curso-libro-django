94793241f9777bf2a91837c31efce582ccc72f29
libredesarrollo <desarrollolibre.net@gmail.com>
2022-03-14 18:00:30 -0400

    varios

--- elements/models.py
+++ elements/models.py
@@ -4,21 +4,21 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
-    url_clean = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
 
     def __str__(self):
         return self.title
 
 class Type(models.Model):
     title = models.CharField(max_length=255)
-    url_clean = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
 
     def __str__(self):
         return self.title
 
 class Element(models.Model):
     title = models.CharField(max_length=255)
-    url_clean = models.SlugField(max_length=255,blank=True)
+    slug = models.SlugField(max_length=255,blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2, default=6.10) # 12345678.10
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
