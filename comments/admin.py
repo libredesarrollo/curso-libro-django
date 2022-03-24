efe0a86e9da7831ec01f3a682a85550ed58c7c4f
libredesarrollo <desarrollolibre.net@gmail.com>
2022-03-24 15:52:51 -0400

    varios

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Comment
+
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','text')
+
+admin.site.register(Comment, CategoryAdmin)
