29e539dab3b7c560de514df5a97f38222e05ceb1
libredesarrollo <desarrollolibre.net@gmail.com>
2022-04-07 18:13:07 -0400

    varios

--- elements/forms.py
+++ elements/forms.py
@@ -4,8 +4,8 @@ from .models import Category, Element, Type
 
 class ElementForm(forms.Form):
 
-    title = forms.CharField(label="Título", max_length=255, min_length=3, validators=[MinLengthValidator(2, message='Muy corto! (minino %(limit_value)d) actual %(show_value)d ')] ) 
-    description = forms.CharField(label='Descripción', initial='Tu increible post por aquí', widget=forms.Textarea)
+    title = forms.CharField(label="Título", max_length=255, min_length=3, validators=[MinLengthValidator(10, message='Muy corto! (minino %(limit_value)d) actual %(show_value)d ')] ) 
+    description = forms.CharField(label='Descripción', initial='Tu increible post por aquí', widget=forms.Textarea, validators=[MinLengthValidator(2, message='Muy corto! (minino %(limit_value)d) actual %(show_value)d ')] )
     price = forms.DecimalField(label="Precio", required=False, max_digits=5, decimal_places=2)    
     type = forms.ModelChoiceField(label='Tipo', queryset=Type.objects.all(), initial=2)
     category = forms.ModelChoiceField(label='Categoría', queryset=Category.objects.all())
